a13bb838e6733fad0ea501ff71e9e01eae5e2fd4
smhrd <smhrd@192.168.219.45>
2024-02-20 19:14:12 +0900

    김영준 - 컨트롤러 레포지토리 커밋

--- src/main/java/com/todaktodak/controller/FeedbackController.java
+++ src/main/java/com/todaktodak/controller/FeedbackController.java
@@ -1,8 +1,34 @@
 package com.todaktodak.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.todaktodak.entity.FeedBack;
+import com.todaktodak.repository.FeedbackRepository;
 
 @Controller
 public class FeedbackController {
+	
+	@Autowired
+	private FeedbackRepository repo;
+
+
+	@ResponseBody
+	@PostMapping("/feedback")
+	public String SaveText(@RequestBody String feedback) {
+		FeedBack backFeed = new FeedBack();
+		System.out.println(feedback);
+		String[] array = feedback.split(":");
+		
+		backFeed.setAiRecommendation(array[0].replaceAll("\"", ""));
+		repo.save(backFeed);
+		
+		
+		
+		return "성공";
+	}
 
 }
